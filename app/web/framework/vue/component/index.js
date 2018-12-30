c27856cb0bd0794ad917b12cc2a4def389e9e866
WiniceS <wiphoton@163.com>
2018-12-30 19:31:59 +0800

    init sms

---
+++ app/web/framework/vue/component/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import AppLayout from 'component/layout/app';
+import iView from 'iview';
+import VueI18n from 'vue-i18n';
+import ElementUi from 'element-ui';
+import EdgarPopover from 'component/popover';
+import EdgarIcon from 'component/icon';
+import 'asset/css/theme.less';
+
+Vue.use(VueI18n);
+Vue.use(iView);
+Vue.use(ElementUi);
+
+Vue.component(AppLayout.name, AppLayout);
+Vue.component(EdgarPopover.name, EdgarPopover);
+Vue.component(EdgarIcon.name, EdgarIcon);
