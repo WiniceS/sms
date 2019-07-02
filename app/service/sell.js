3f98e19d9dc270b9f707942acdf3d423f7e6cc93
WiniceS <wiphoton@163.com>
2019-07-03 00:05:27 +0800

    更新sell对数据库的操作

--- app/service/sell.js
+++ app/service/sell.js
@@ -3,94 +3,84 @@ const egg = require('egg');
 module.exports = class SellService extends egg.Service {
     // 添加销售记录
     async sell(sellList) {
-        const result = []
-        sellList.forEach( e => {
-            const tmpResult = this.sellSingle(e)
-            result.push(tmpResult)
-        });
-        if (result.length > 0 && result.every(every => every.success === true)) {
-            return {
-                success: true
-            }
-        } else {
-            return {
-                result
-            }
+        // 获取物料数量等于0的商品
+        const zeroRecode = await this.app.mysql.select('v_good_info', {
+            where: {
+                good_number: 0
+            },
+            columns: ['good_id']
+        })
+        let fatherGoodList = []
+        if (zeroRecode != null && zeroRecode.length > 0) {
+            fatherGoodList = sellList.filter(f => {
+                if (f.havfather == 1) {
+                    return zeroRecode.some(s => s.good_id = f.father_good_id)
+                } else {
+                    return false
+                }
+            })
         }
-
-    }
-    // 销售单个（不想拼接SQL语句）
-    async sellSingle(sellInfo) {
-        const tmpResult = await this.app.mysql.beginTransactionScope(async conn => {
-            // 获取销售信息
-            const sellRecode = {
-                good_id: sellInfo.good_id,
-                sell_num: sellInfo.good_sell_number,
-                unit_price: sellInfo.good_sell,
-                total_price: sellInfo.subtotal,
-                sell_date: sellInfo.sell_date,
-                discounts: sellInfo.discounts,
-                earnings: sellInfo.subtotal - sellInfo.good_sell_number * sellInfo.good_cost
+        // 获取销售信息
+        const sellRecode = sellList.map(e => {
+            const tmp = {
+                good_id: e.good_id,
+                sell_num: e.good_sell_number,
+                unit_price: e.good_sell,
+                total_price: e.subtotal,
+                sell_date: this.app.mysql.literals.now,
+                discounts: e.discounts,
+                earnings: e.subtotal - e.good_sell_number * e.good_cost
             }
+            return tmp
+        });
+        // 更新商品数量
+        const Literal = this.app.mysql.literals.Literal;
+        // 更新商品数量
+        const numberRecode = sellList.map(m => {
+            let number = m.ratio * Math.ceil(m.good_sell_number / m.ratio) - m.good_sell_number
+            let updateRecode = {
+                row: {
+                    good_number: m.havfather === '1' ? new Literal(`IF(good_number - ${m.good_sell_number}>-1,good_number -${m.good_sell_number},good_number + ${number})`) : new Literal(`IF(good_number - ${m.good_sell_number}>0,good_number - ${m.good_sell_number},0)`)
+                },
+                where: {
+                    good_id: m.good_id
+                }
+            }
+            return updateRecode
+        })
+        // 更新如果子商品为0的时候，父商品的数量
+        const fatherRecode = fatherGoodList.length > 0 ? fatherGoodList.map(m => {
+            const number = Math.ceil(m.good_sell_number / m.ratio)
+            let updateRecode = {
+                row: {
+                    good_number: new Literal(`IF(good_number - ${number}>0,good_number - ${number},0)`)
+                },
+                where: {
+                    good_id: m.father_good_id
+                }
+            }
+            return updateRecode
+        }) : []
+
+
+        const result = await this.app.mysql.beginTransactionScope(async conn => {
+
             // 插入数据表
             await conn.insert('tb_sell', sellRecode)
-
-            if (sellInfo.havfather === 1) {
-                // 如果商品有父商品
-                const fatherGoodNumberList = await this.app.mysql.get('v_good_info', {
-                    good_id: sellInfo.father_good_id
-                })
-                const goodNumberList = await this.app.mysql.get('v_good_info', {
-                    good_id: sellInfo.good_id
-                })
-                const options = {
-                    where: {
-                        good_id: sellInfo.good_id
-                    }
-                }
-                const options2 = {
-                    where: {
-                        good_id: sellInfo.father_good_id
-                    }
-                }
-                const row = {
-                    good_number: goodNumberList[0].good_number - 1
-                }
-                // 判断子商品是否卖完了，是从父商品中拆出来的
-                if (goodNumberList[0].good_number == 0) {
-                    // 将子父商品的数量都更改
-                    row = {
-                        good_number: ratio - 1
-                    }
-                    const row2 = {
-                        good_number: fatherGoodNumberList[0].good_number - 1
-                    }
-                    await conn.update('tb_inventory', row, options)
-                    await conn.update('tb_inventory', row2, options2)
-                } else {
-                    // 只需更改子商品的数量
-                    await conn.update('tb_inventory', row, options)
-                }
-            } else {
-                // 更新商品数量
-                const goodNumberList = await this.app.mysql.get('v_good_info', {
-                    good_id: sellInfo.good_id
-                })
-                const row = {
-                    good_number: goodNumberList[0].good_number - 1
-                }
-                const options = {
-                    where: {
-                        good_id: sellInfo.good_id
-                    }
-                }
-                await conn.update('tb_inventory', row, options)
+            if (numberRecode.length > 0) {
+                await conn.updateRows('tb_inventory', numberRecode)
+            }
+            if (fatherRecode.length > 0) {
+                await conn.updateRows('tb_inventory', fatherRecode)
             }
             return {
                 success: true
             }
-        }, ctx)
-        tmpResult.id = sellInfo.good_id
-        return tmpResult
+        }, this.ctx)
+
+        return {
+            result
+        }
     }
+
 };
